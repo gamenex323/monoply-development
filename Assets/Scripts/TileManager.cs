babf103731e7d53e31b27dbc2171ef0f6c51404d
gamenex323 <zohaibgithubprojects@gmail.com>
2024-12-19 02:00:42 +0500

    CommuniytChest, Chances

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -24,10 +24,32 @@ public class TileManager : MonoBehaviour
     public TextMeshProUGUI attack;
 
     [Header("CommunityChest Reward")]
-    public TextMeshProUGUI GiveCommunityChestText;
+    public GameObject communityResourcesPanel;
+    public TextMeshProUGUI communityResourceTitle;
+    public TextMeshProUGUI communityResourceDesciption;
+
+    public Chances[] communityResourcesData;
 
     [Header("Chance Reward")]
-    public GameObject[] ChancePanels;
+    public GameObject ChancePanels;
+    public TextMeshProUGUI chanceTitle;
+    public TextMeshProUGUI chanceDescription;
+
+    public Chances[] communityChancesData;
+    [System.Serializable]
+    public class Chances
+    {
+        public string Title;
+        public string Description;
+        public int money;
+    }
+    [System.Serializable]
+    public class CommunityResource
+    {
+        public string Title;
+        public string Description;
+        public int money;
+    }
     public enum ChanceType
     {
         GrandFatherGaveMoney,
@@ -123,16 +145,20 @@ public class TileManager : MonoBehaviour
     }
     private void GiveCommunityChest()
     {
-        PlayerPrefs.SetInt(GlobalData.CommmunityChest, PlayerPrefs.GetInt(GlobalData.CommmunityChest) + 1);
-        GiveCommunityChestText.text = PlayerPrefs.GetInt(GlobalData.CommmunityChest).ToString();
+        int rndChance = UnityEngine.Random.Range(0, communityResourcesData.Length);
+        communityResourceDesciption.text = communityResourcesData[rndChance].Description;
+        communityResourceTitle.text = communityResourcesData[rndChance].Title;
+        UIManager.instance.UpdateMoney(communityResourcesData[rndChance].money);
         // Implement logic for shield reward
-        Debug.Log("Giving CommmunityChest reward!");
+        Debug.Log("Giving Community Resource Reward!");
     }
 
     private void GiveChance()
     {
-        int rndChance = UnityEngine.Random.Range(0, ChancePanels.Length);
-        ChancePanels[rndChance].SetActive(true);
+        int rndChance = UnityEngine.Random.Range(0, communityChancesData.Length);
+        chanceDescription.text = communityChancesData[rndChance].Description;
+        chanceTitle.text = communityChancesData[rndChance].Title;
+        UIManager.instance.UpdateMoney(communityChancesData[rndChance].money);
         // Implement logic for shield reward
         Debug.Log("Giving Chance reward!");
     }
